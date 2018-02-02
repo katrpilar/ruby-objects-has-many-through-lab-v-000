9cab89973cd86c51bdb99c153513f7f9c07bfda4
Katrina Brinson <kbrinso@gmail.com>
2018-02-02 21:59:44 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,14 +1,23 @@
 class Genre 
+<<<<<<< HEAD
   attr_accessor :songs, :artists, :name
   
   def initialize(name)
     @name = name
+=======
+  attr_accessor :songs, :artists
+  
+  def initialize(name)
+>>>>>>> 901077deace37859de8c4ef68142b899a05dc557
     @songs = []
   end
   
   def add_song(song)
     @songs << song
+<<<<<<< HEAD
     song.genre = self
+=======
+>>>>>>> 901077deace37859de8c4ef68142b899a05dc557
   end
   
   def artists
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
 class Song
+=======
+def Song
+>>>>>>> 901077deace37859de8c4ef68142b899a05dc557
   attr_accessor :name, :genre, :artist
   
   def initialize(name, genre)
     @name = name 
     @genre = genre
+<<<<<<< HEAD
     genre.add_song(self)
+=======
+    genre.add_song = self
+>>>>>>> 901077deace37859de8c4ef68142b899a05dc557
   end
     
 end
